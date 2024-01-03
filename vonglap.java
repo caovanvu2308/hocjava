261d4ab5ebe241be6f23f6a2a8d07c74512d0de2
caovanvu2308 <caovanvu2308@gmail.com>
2024-01-04 00:15:36 +0700

    0h15

--- vonglap.java
+++ vonglap.java
@@ -1,24 +1,22 @@
 import java.util.Scanner;
 public class vonglap{
     public static void main (String []args){
-        Scanner nhap = new Scanner(System.in);
-        System.out.print("Hay nhap so ma ban muon lap: ");
-        String nhapvao = nhap.nextLine();
-        // Tạo một đối tượng Scanner để đọc dữ liệu từ bàn phím
         Scanner scanner = new Scanner(System.in);
 
-        // Nhập chuỗi từ bàn phím
-        System.out.print("Nhập chuỗi: ");
-        String userInput = scanner.nextLine();
+        // Nhập số nguyên từ bàn phím
+        System.out.print("Nhập một số nguyên: ");
+        int userNumber = scanner.nextInt();
+
+        // In ra số nguyên đã nhập
+        System.out.println("Số nguyên bạn vừa nhập là: " + userNumber);
+
         // Đóng đối tượng Scanner sau khi sử dụng
-        scanner.close();
-    
+        System.out.println("In ra các số từ 1 đến " + userNumber + " trong vòng lặp:");
 
-
-
-        for (int i=1; i<10;i++){
-             System.out.println("Xin chao day la 10 vong lap: "+ i);
+        for (int i = 1; i <= userNumber; i++) {
+            System.out.println("vong lap lan thu "+i);
         }
+
     
     }
 }
