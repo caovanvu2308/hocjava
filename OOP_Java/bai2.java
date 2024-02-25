467a309e3ec8c93b601c29556802c2c93171c15f
caovanvu2308 <caovanvu2308@gmail.com>
2024-02-25 14:53:46 +0700

    add

---
+++ OOP_Java/bai2.java
@@ -0,0 +1,10 @@
+public class bai2 {
+    // hàm tạo có đối số
+    public bai2(String name) {
+    System.out.println("Your name: " + name);
+    }
+    public static void main(String[] args) {
+    // Khởi tạo đối tượng với giá trị truyền vào là "Freetuts"
+    bai2 bai2 = new bai2("Freetuts");
+    }
+}
--- ifelse.java
+++
@@ -1,6 +0,0 @@
-public class ifelse {
-    public static void main(String[] args) {
-        // Mã thực thi của bạn ở đây
-        System.out.println("Hello from ifelse class!");
-    }
-}
